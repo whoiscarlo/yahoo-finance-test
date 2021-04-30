bddb84c7b636c5afe6a6db227c27fdbb86bd5693
Carlo Nyte <8445082+whoiscarlo@users.noreply.github.com>
2021-04-30 11:57:26 -0700

    ^ code update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { from, Observable } from 'rxjs';
+import yahooFinance from 'yahoo-finance2';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'yahoo-finance-test';
+export class AppComponent implements OnInit{
+  symbol = 'FB';
+  quote$!: Observable<any>;
+
+  ngOnInit() {
+    this.quote$ = from(yahooFinance.quote(this.symbol));
+  }
 }
